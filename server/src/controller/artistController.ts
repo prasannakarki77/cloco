c1732559060159bf99ca895b9072142ac030cbc3
Prasanna Raj Karki <63549402+prasannakarki77@users.noreply.github.com>
2024-08-04 20:58:03 +0545

    get artists route response update

--- server/src/controller/artistController.ts
+++ server/src/controller/artistController.ts
@@ -74,8 +74,39 @@ export const updateArtist = async (req: Request, res: Response) => {
 
 export const getArtists = async (req: Request, res: Response) => {
   try {
-    const artists = await pool.query("SELECT * FROM artists");
-    res.status(200).json(artists.rows);
+    const page = parseInt(req.query.page as string) || 1;
+    const pageSize = parseInt(req.query.pageSize as string) || 10;
+    const offset = (page - 1) * pageSize;
+
+    const artistsQuery = `
+      SELECT * FROM artists
+      ORDER BY id
+      LIMIT $1 OFFSET $2
+    `;
+    const { rows: artists } = await pool.query(artistsQuery, [
+      pageSize,
+      offset,
+    ]);
+
+    const countQuery = `SELECT COUNT(*) FROM artists`;
+    const {
+      rows: [{ count }],
+    } = await pool.query(countQuery);
+    const totalCount = parseInt(count, 10);
+
+    const totalPages = Math.ceil(totalCount / pageSize);
+    const hasNextPage = page < totalPages;
+    const hasPrevPage = page > 1;
+
+    res.status(200).json({
+      data: artists,
+      page,
+      pageSize,
+      totalPages,
+      totalCount,
+      hasNextPage,
+      hasPrevPage,
+    });
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Failed to fetch artists" });
