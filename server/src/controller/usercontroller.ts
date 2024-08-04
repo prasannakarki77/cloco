df1a29ae3d00a59fa9cf141f24daa887f0fc00c0
Prasanna Raj Karki <63549402+prasannakarki77@users.noreply.github.com>
2024-08-04 13:50:00 +0545

    user controller and router

--- server/src/controller/usercontroller.ts
+++ server/src/controller/usercontroller.ts
@@ -1,21 +1,11 @@
-import { Request, Response } from "express";
 import pool from "../config/db";
 import { User } from "../model/user";
-import bcrypt from "bcrypt";
-import jwt, { JwtPayload } from "jsonwebtoken";
+import { Request, Response } from "express";
 
 export const createUser = async (req: Request, res: Response) => {
   try {
-    const {
-      first_name,
-      last_name,
-      email,
-      password,
-      phone,
-      gender,
-      address,
-      dob,
-    } = req.body;
+    const { first_name, last_name, email, phone, gender, address, dob } =
+      req.body;
 
     const userExists = await pool.query(
       "SELECT * FROM users WHERE email = $1",
@@ -23,27 +13,16 @@ export const createUser = async (req: Request, res: Response) => {
     );
 
     if (userExists.rows.length > 0) {
-      return res.status(400).json({ error: "Email already exists" });
+      return res
+        .status(400)
+        .json({ error: "User with this email already exists" });
     }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
     const result = await pool.query(
-      `INSERT INTO users (first_name, last_name, email, password, phone, gender, address, dob)
-       VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
-       RETURNING *`,
-      [
-        first_name,
-        last_name,
-        email,
-        hashedPassword,
-        phone,
-        gender,
-        address,
-        dob,
-      ]
+      `INSERT INTO users (first_name, last_name, email, phone, gender, address, dob)
+         VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+         RETURNING *`,
+      [first_name, last_name, email, phone, gender, address, dob]
     );
-
     const newUser: User = result.rows[0];
     res.status(201).json(newUser);
   } catch (err) {
@@ -52,39 +31,74 @@ export const createUser = async (req: Request, res: Response) => {
   }
 };
 
-export const loginUser = async (req: Request, res: Response): Promise<void> => {
-  const { email, password } = req.body;
-  const JWT_SECRET = process.env.JWT_SECRET || "";
-
+export const updateUserById = async (req: Request, res: Response) => {
   try {
-    const result = await pool.query("SELECT * FROM users WHERE email = $1", [
-      email,
-    ]);
+    const { id } = req.params;
+    const { first_name, last_name, email, phone, gender, address, dob } =
+      req.body;
 
-    if (result.rows.length === 0) {
-      res.status(400).json({ error: "Invalid email or password" });
-      return;
+    if (
+      !first_name ||
+      !last_name ||
+      !email ||
+      !phone ||
+      !gender ||
+      !address ||
+      !dob
+    ) {
+      return res.status(400).json({ error: "All fields are required" });
+    }
+    const user = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+    if (user.rows.length === 0) {
+      return res.status(404).json({ error: "User not found" });
     }
 
-    const user = result.rows[0];
-
-    const isMatch = await bcrypt.compare(password, user.password);
-
-    if (!isMatch) {
-      res.status(400).json({ error: "Invalid email or password" });
-      return;
-    }
-
-    const token = jwt.sign(
-      { id: user.id, email: user.email } as JwtPayload,
-      JWT_SECRET,
-      {
-        expiresIn: "24h",
-      }
+    const result = await pool.query(
+      `UPDATE users
+         SET first_name = $1, last_name = $2, email = $3, phone = $4, gender = $5, address = $6, dob = $7
+         WHERE id = $8
+         RETURNING *`,
+      [first_name, last_name, email, phone, gender, address, dob, id]
     );
 
-    res.json({ token });
+    const updatedUser = result.rows[0];
+    res.status(200).json(updatedUser);
   } catch (err) {
-    res.status(500).json({ error: "Failed to log in" });
+    console.error(err);
+    res.status(500).json({ error: "Failed to update user" });
+  }
+};
+
+export const getUsers = async (req: Request, res: Response) => {
+  try {
+    const users = await pool.query("SELECT * FROM users");
+    res.status(201).json(users.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to create user" });
+  }
+};
+
+export const deleteUserById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    if (!id) {
+      return res.status(400).json({ error: "User ID is required" });
+    }
+    const result = await pool.query(
+      "DELETE FROM users WHERE id = $1 RETURNING *",
+      [id]
+    );
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    res
+      .status(200)
+      .json({ message: "User deleted successfully", user: result.rows[0] });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to delete user" });
   }
 };
--- server/src/router/userRouter.ts
+++ server/src/router/userRouter.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
-import { createUser, loginUser } from "../controller/usercontroller";
+import {
+  createUser,
+  deleteUserById,
+  getUsers,
+  updateUserById,
+} from "../controller/userController";
 
 const userRouter = Router();
 
-userRouter.post("/register", createUser);
-userRouter.post("/login", loginUser);
+userRouter.post("/create", createUser);
+userRouter.patch("/update/:id", updateUserById);
+userRouter.delete("/delete/:id", deleteUserById);
+userRouter.get("/get-all", getUsers);
 
 export default userRouter;
