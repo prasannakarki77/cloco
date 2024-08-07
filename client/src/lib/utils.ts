c410e949d41cda8ec573aea05dabed04dc34b05c
Prasanna Raj Karki <63549402+prasannakarki77@users.noreply.github.com>
2024-08-07 15:02:13 +0545

    date fns fomat util

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,8 +1,15 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { format } from "date-fns";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
 export const API_URL = process.env.NEXT_PUBLIC_API_URL || "";
+
+export function fDate(date: Date | string, newFormat?: string) {
+  const fm = newFormat || "MMMM d, yyyy";
+
+  return date ? format(new Date(date), fm) : "";
+}
