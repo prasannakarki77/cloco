05f6f8b2dad6613f7929c2ceb44228cff01b11ce
Prasanna Raj Karki <63549402+prasannakarki77@users.noreply.github.com>
2024-08-07 16:12:11 +0545

    artist context update for musics

--- client/src/context/ArtistContext.tsx
+++ client/src/context/ArtistContext.tsx
@@ -1,13 +1,15 @@
 import { createContext, useState, ReactNode } from "react";
 import axios from "axios";
-import { Artist, GetArtistsResponse } from "@/types";
+import { GetArtistsResponse, Music } from "@/types";
 import { API_URL } from "@/lib/utils";
 
 interface ArtistContextProps {
   artistsRes: GetArtistsResponse | null;
+  artistMusics: Music[] | null;
   loading: boolean;
   error: string | null;
   fetchArtistData: () => void;
+  fetchArtistMusic: (id: string) => void;
   page: number;
   pageSize: string;
   setPage: React.Dispatch<React.SetStateAction<number>>;
@@ -16,9 +18,11 @@ interface ArtistContextProps {
 
 export const ArtistContext = createContext<ArtistContextProps>({
   artistsRes: null,
+  artistMusics: null,
   loading: true,
   error: null,
   fetchArtistData: () => {},
+  fetchArtistMusic: () => {},
   page: 1,
   pageSize: "5",
   setPage: () => {},
@@ -29,6 +33,8 @@ export const ArtistProvider: React.FC<{ children: ReactNode }> = ({
   children,
 }) => {
   const [artistsRes, setArtistsRes] = useState<GetArtistsResponse | null>(null);
+  const [artistMusics, setArtistMusics] = useState<Music[] | null>(null);
+
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
   const [page, setPage] = useState<number>(1);
@@ -48,6 +54,18 @@ export const ArtistProvider: React.FC<{ children: ReactNode }> = ({
     }
   };
 
+  const fetchArtistMusic = async (id: string) => {
+    setLoading(true);
+    try {
+      const res = await axios.get(`${API_URL}/artist/music/get-all/${id}`);
+      setArtistMusics(res.data.musics);
+    } catch (error: any) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <ArtistContext.Provider
       value={{
@@ -55,6 +73,8 @@ export const ArtistProvider: React.FC<{ children: ReactNode }> = ({
         loading,
         error,
         fetchArtistData,
+        fetchArtistMusic,
+        artistMusics,
         page,
         pageSize,
         setPageSize,
